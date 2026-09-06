39cde9da34aed42fef96cd7ac3067d705b864518
ShrawaniPatil11 <shravanip163@gmail.com>
2026-09-07 00:13:12 +0530

    Time: 0 ms (100%), Space: 8.8 MB (88.91%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
-    bool isValid(std::string s) {
-        std::stack<char> stk;
-        
-        for (char c : s) {
-            // If it's an opening bracket, push it onto the stack
-            if (c == '(' || c == '{' || c == '[') {
-                stk.push(c);
-            } 
-            // If it's a closing bracket, check for a valid match
-            else {
-                // If the stack is empty, there's no matching opening bracket
-                if (stk.empty()) return false;
-                
-                char top = stk.top();
-                if ((c == ')' && top == '(') ||
-                    (c == '}' && top == '{') ||
-                    (c == ']' && top == '[')) {
-                    stk.pop(); // Found a match, remove the opening bracket
-                } else {
-                    return false; // Mismatched bracket type
+    bool isValid(string str) {
+        stack<char> st;
+        for(int i=0;i<str.size();i++)
+        {
+            if(str[i]== '{' || str[i]=='[' || str[i]=='('){
+                st.push(str[i]);
+            }
+            else{
+                if(st.size() ==0){
+                    return false;
                 }
+
+                if((st.top() =='(' && str[i]==')') || (st.top() =='[' && str[i]==']') ||(st.top() =='{' && str[i]=='}')){
+                    st.pop();
+                }
+                else{
+                    return false;
+                }
+                
+                
             }
         }
-        
-        // If the stack is empty, all brackets were properly matched and closed
-        return stk.empty();
-    }
+        return st.size() == 0;
+
+    } 
+    
 };
